bc3fa80ede3c881c453456c8109b82f816580824
anneuf <anneuf@cisco.com>
2016-01-24 11:01:08 +0100

    Update 3

--- conf/install_wpa.sh
+++ conf/install_wpa.sh
@@ -1,9 +1,13 @@
+SSID="<Put SSID here>"
+PSK="<Put PSK here>"
+
+
 echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" > /etc/wpa_supplicant/wpa_supplicant.conf
 echo "update_config=1" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo " " >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "network={" >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo "        ssid=\"winwets\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo "        psk=\"IrrerIvan\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "        ssid=\"$SSID\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "        psk=\"$PSK\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "        proto=RSN" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "        key_mgmt=WPA-PSK" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "        pairwise=CCMP" >> /etc/wpa_supplicant/wpa_supplicant.conf
@@ -11,8 +15,8 @@ echo "        auth_alg=OPEN" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "}" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo " " >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "network={" >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo "        ssid=\"winwets\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo "        psk=\"IrrerIvan\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "        ssid=\"$SSID\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "        psk=\"$PSK\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "        proto=RSN" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "        key_mgmt=WPA-PSK" >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo "        pairwise=TKIP" >> /etc/wpa_supplicant/wpa_supplicant.conf
